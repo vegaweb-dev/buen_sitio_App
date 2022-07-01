97f144568d7f3a004083948705672c4b450b4e11
carlos <carlosjuniorrvega@hotmail.com>
2022-07-01 13:29:17 -0500

    initial_phase:setting up linter and formatter

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		jest: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
+	settings: {
+		react: { version: 'detect' },
+	},
+};
